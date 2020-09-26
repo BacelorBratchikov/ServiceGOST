fd984933b2812be0037538e9725d62931423356c
RUGyron <32088165+RUGyron@users.noreply.github.com>
2020-09-27 00:55:03 +0300

    new release

--- docfx/serve.bat
+++ docfx/serve.bat
@@ -2,4 +2,4 @@ if exist _site rmdir /s /q _site
 if exist obj rmdir /s /q obj
 if exist api rmdir /s /q api
 
-C:\Programs\docfx\docfx.exe docfx.json --serve   -p 8081
+C:\Users\RUGyron\Documents\GitHub\ServiceGOST\docfx\docfx.exe docfx.json --serve   -p 8081
